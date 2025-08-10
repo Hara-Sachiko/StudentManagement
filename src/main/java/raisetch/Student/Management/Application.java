ef18985911c50886428fcc13f68d150ca3956d20
Hara Sachiko <l2311795446as@gmail.com>
2025-08-10 14:03:20 +0900

    課題５

--- src/main/java/raisetch/Student/Management/Application.java
+++ src/main/java/raisetch/Student/Management/Application.java
@@ -2,24 +2,43 @@ package raisetch.Student.Management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private Map<String, String> student = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello, World!";
+	// 初期値の設定（コンストラクタ）
+	public Application() {
+		student.put("name", "Sachiko Hara");
+		student.put("age", "44");
 	}
 
-	@GetMapping("/goodmorning")
-	public String goodmorning(){
-		return "Hello, Goodmorning!";
+	// GET: 学生情報を取得
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return student.get("name") + " " + student.get("age") + " sai";
+	}
+
+	// POST: 名前と年齢を一緒に更新
+	@PostMapping("/name")
+	public void setStudentInfo(@RequestParam String name, @RequestParam String age) {
+		student.put("name", name);
+		student.put("age", age);
+	}
+
+	// POST: 名前だけを更新
+	@PostMapping("/studentName")
+	public void updateStudentName(@RequestParam String name) {
+		student.put("name", name);
 	}
 }
